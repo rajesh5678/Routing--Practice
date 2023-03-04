68b4ee03ea0c0ac466480d249792e5712c5f69d7
Rajesh <rajeshkuarsun56@gmail.com>
2023-03-04 06:38:17 +0000

    done

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+
+const Home = () => (
+  <div className="home-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home"
+    />
+    <h1 className="header">Home</h1>
+  </div>
+)
+
+export default Home
